21458f95e88cb2e140d104e945db38931f396152
Vinit Patel <patel.vinit@northeastern.edu>
2024-02-04 23:28:54 -0500

    tasks 1 and 2 submit

--- sh3650.c
+++ sh3650.c
@@ -22,6 +22,10 @@
 #include <signal.h>
 #include <fcntl.h>
 
+int cd_cmd(int argc, char **argv);
+int pwd_cmd(int argc, char **argv);
+int exit_cmd(int argc, char **argv);
+
 
 int main(int argc, char **argv)
 {
@@ -41,9 +45,12 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    signal(SIGINT, SIG_IGN); /* ignore SIGINT=^C */
+
     char line[1024], linebuf[1024];
     const int max_tokens = 32;
     char *tokens[max_tokens];
+    int status = 0;
 
     /* loop:
      *   if interactive: print prompt
@@ -71,12 +78,75 @@ int main(int argc, char **argv)
 
         /* replace the code below with your shell:
          */
-        printf("line:");
-        for (int i = 0; i < n_tokens; i++)
-            printf(" '%s'", tokens[i]);
-        printf("\n");
+	if (n_tokens > 0) {
+	    if (strcmp(tokens[0], "cd") ==0) {
+		status = cd_cmd(n_tokens, tokens);
+	    } else if (strcmp(tokens[0], "pwd") == 0) {
+		status = pwd_cmd(n_tokens, tokens);
+	    } else if (strcmp(tokens[0], "exit") == 0) {
+		status = exit_cmd(n_tokens, tokens);
+	    } else {
+       		printf("line:");
+       		for (int i = 0; i < n_tokens; i++)
+            	    printf(" '%s'", tokens[i]);
+        	printf("\n");
+	    }
+    	}
     }
 
     if (interactive)            /* make things pretty */
         printf("\n");           /* try deleting this and then quit with ^D */
+
+    return status;
 }
+
+
+int cd_cmd(int argc, char **argv) {
+        if (argc > 2) {
+            fprintf(stderr, "cd: wrong number of arguments\n");
+            return 1;
+        }
+
+        const char *dir = (argc == 1) ? getenv("HOME") : argv[1];
+
+        if (chdir(dir) == -1) {
+           fprintf(stderr, "cd: %s\n", strerror(errno));
+           return 1;
+        }
+
+        return 0;
+
+    }
+
+
+int pwd_cmd(int argc, char **argv) {
+        if (argc > 2) {
+            fprintf(stderr, "pwd: too many arguments\n");
+            return 1;
+        }
+
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            perror("pwd");
+            return 1;
+        }
+
+        printf("%s\n", cwd);
+        return 0;
+    }
+
+
+int exit_cmd(int argc, char **argv) {
+        if (argc > 2) {
+            fprintf(stderr, "exit: too many arguments\n");
+            return 1;
+        }
+
+
+        if (argc == 1) {
+            exit(0);
+        } else {
+            int status = atoi(argv[1]);
+            exit(status);
+        }
+    }
