618af39ba79b35b3005eb8f28b3c3f74cd54536f
Vinit Patel <patel.vinit@northeastern.edu>
2024-02-05 22:07:59 -0500

    revised status

--- sh3650.c
+++ sh3650.c
@@ -108,17 +108,17 @@ int main(int argc, char **argv)
 int cd_cmd(int argc, char **argv) {
         if (argc > 2) {
             fprintf(stderr, "cd: wrong number of arguments\n");
-            return 1;
+            return 1; // exit status if i call cd 1 2, wrong no. of arguments
         }
 
         const char *dir = (argc == 1) ? getenv("HOME") : argv[1];
 
         if (chdir(dir) == -1) {
            fprintf(stderr, "cd: %s\n", strerror(errno));
-           return 1;
+           return 1; // exit status if i call cd /not-real, aka fake dir 
         }
 
-        return 0;
+        return 0; // a successful cd
 
     }
 
@@ -162,7 +162,7 @@ void exec_cmd(int argc, char **argv) {
 	perror("fork");
 	exit(EXIT_FAILURE);
     } else if (pid == 0) { // the child process
-	// re-enabling ctrl C
+	// re-enabling ctrl C according to part 1 instructions
 	signal(SIGINT, SIG_DFL);
 
 	if (execvp(argv[0], argv) == -1) {
@@ -171,9 +171,7 @@ void exec_cmd(int argc, char **argv) {
 	}
     } else { // parent process
 	int status;
-	do {
-	    waitpid(pid, &status, WUNTRACED);
-	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	waitpid(pid, &status, 0);
 
 	if (WIFEXITED(status)) {
 	    int exit_status = WEXITSTATUS(status);
@@ -196,7 +194,7 @@ void replace_status(char **tokens, int n_tokens) {
     sprintf(exit_status_str, "%d", last_status);
 
     for (int i = 0; i < n_tokens; i++) {
-	if (strcmp(tokens[i], "$?") ==0) {
+	if (strcmp(tokens[i], "$?") == 0) {
 	    tokens[i] = exit_status_str;
 	}
     }
